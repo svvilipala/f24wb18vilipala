fd2e2ae43eefea451067726b60afbeab1cb59f2e
Sri Vidya <svvilipala@gmail.com>
2024-11-05 17:01:48 -0600

    Added grid file to routes and views

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');   
 var spicesRouter = require('./routes/spices'); 
+var gridRouter = require('./routes/grid'); 
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/spices', spicesRouter);
+app.use('/grid', gridRouter);
 
 
 // catch 404 and forward to error handler
