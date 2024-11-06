0a3a749abff5bb8c253bf168b0cf3c247aa2948b
Sri Vidya <svvilipala@gmail.com>
2024-11-05 18:32:31 -0600

    Added grid file

--- routes/grid.js
+++ routes/grid.js
@@ -3,11 +3,14 @@ var router = express.Router();
 
 /* GET Grid page.*/
 router.get('/', function(req, res) {
-    let query = req.query;
-    console.log(`rows ${query.rows}`)
-    console.log(`cols ${query.cols}`)
+    const rows = parseInt(req.query.rows, 10);
+    const cols = parseInt(req.query.cols, 10);
+    
+    // console.log(`rows ${query.rows}`)
+    // console.log(`cols ${query.cols}`)
 
-  res.render('grid', { title: "Grid Display", query: query });
+//   res.render('grid', { title: "Grid Display", query: query });
+  res.render('grid', { query: { rows, cols } });
 });
 
 module.exports = router;
